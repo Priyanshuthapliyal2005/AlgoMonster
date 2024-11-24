53c22ea1bce566e81d60c833787d3c1b06476d55
Priyanshu Thapliyal <priyanshuthapliyal2005@gmail.com>
2024-11-24 23:00:43 +0530

    done

--- components/SideNav/sub-component/NavCategory.jsx
+++ components/SideNav/sub-component/NavCategory.jsx
@@ -16,7 +16,7 @@ const NavCategory = () => {
             key={data.id + data.algorithms[0].id}
             className="nav-category-container flex-col px-4 py-[15px] w-full bg-bg-2 border-2 border-border-1 cursor-pointer"
           >
-            <h2 className="font-space uppercase text-blue text-center text-[1.1rem] cursor-pointer mb-[5px]">
+            <h2 className="font-space uppercase text-green text-center text-[1.1rem] cursor-pointer mb-[5px]">
               {data.name}
             </h2>
             <div
@@ -33,11 +33,11 @@ const NavCategory = () => {
                     href={"/" + data.id + "/" + algorithm.id}
                   >
                     {currAlgoId != algorithm.id ? (
-                      <div className="w-full border-2 border-bg-2 hover:border-blue hover:border-2 hover:bg-blue-bg font-space text-text-1 text-center text-[0.95rem] ">
+                      <div className="w-full border-2 border-bg-2 hover:border-green hover:border-2 hover:bg-green-bg font-space text-text-1 text-center text-[0.95rem] ">
                         {algorithm.name}
                       </div>
                     ) : (
-                      <div className="w-full border-2 border-bg-2 bg-blue-bg-4 hover:border-blue font-space text-text-1 text-center text-[0.95rem] ">
+                      <div className="w-full border-2 border-bg-2 bg-green-bg-4 hover:border-green font-space text-text-1 text-center text-[0.95rem] ">
                         {algorithm.name}
                       </div>
                     )}
--- pages/_document.js
+++ pages/_document.js
@@ -14,80 +14,62 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Unica+One&display=swap"
           rel="stylesheet"
         />
+        {/* Updated icons */}
         <link
           rel="apple-touch-icon"
-          sizes="57x57"
-          href="/favicon/apple-icon-57x57.png"
+          sizes="16x16"
+          href="/favicon/icons8-monster-color-16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="32x32"
+          href="/favicon/icons8-monster-color-32.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="60x60"
-          href="/favicon/apple-icon-60x60.png"
+          href="/favicon/icons8-monster-color-60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="70x70"
+          href="/favicon/icons8-monster-color-70.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="72x72"
-          href="/favicon/apple-icon-72x72.png"
+          href="/favicon/icons8-monster-color-72.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="76x76"
-          href="/favicon/apple-icon-76x76.png"
+          href="/favicon/icons8-monster-color-76.png"
         />
         <link
           rel="apple-touch-icon"
-          sizes="114x114"
-          href="/favicon/apple-icon-114x114.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="/favicon/apple-icon-120x120.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="144x144"
-          href="/favicon/apple-icon-144x144.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/favicon/apple-icon-152x152.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-icon-180x180.png"
-        />
-        <link
-          rel="shortcut icon"
-          type="image/png"
-          sizes="192x192"
-          href="/favicon/android-icon-192x192.png"
+          sizes="96x96"
+          href="/favicon/icons8-monster-color-96.png"
         />
         <link
           rel="shortcut icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon/favicon-32x32.png"
+          href="/favicon/icons8-monster-color-32.png"
         />
         <link
           rel="shortcut icon"
           type="image/png"
           sizes="96x96"
-          href="/favicon/favicon-96x96.png"
+          href="/favicon/icons8-monster-color-96.png"
         />
         <link
-          rel="shortcut icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
+          rel="manifest"
+          href="/favicon/manifest.json"
         />
-        <link rel="manifest" href="/favicon/manifest.json" />
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta
           name="msapplication-TileImage"
-          content="/favicon/ms-icon-144x144.png"
+          content="/favicon/icons8-monster-color-70.png"
         />
         <meta name="theme-color" content="#111" />
       </Head>
